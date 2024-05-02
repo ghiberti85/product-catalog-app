e39ee4823d29287fde3b461c050e3f5f1a620e56
ghiberti85 <ghiberti85@gmail.com>
2024-05-01 23:27:13 -0300

    feat: app component & product service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,11 @@
-import { Component, NgModule } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
-
-export class AppModule { }
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-
-
 export class AppComponent {
-product: any;
-filterProducts() {
-throw new Error('Method not implemented.');
-}
-  title = 'product-catalog-app';
-searchTerm: any;
-
+  title = 'Infinite summer ice cream store';
+  subtitle = 'Which one do you want?';
 }
