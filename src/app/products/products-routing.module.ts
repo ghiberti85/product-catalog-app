620272000ac1d6afb1f0fdfc1e0822ed61663a03
ghiberti85 <ghiberti85@gmail.com>
2024-05-07 00:32:25 -0300

    Product Routing Module

---
+++ src/app/products/products-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+
+const routes: Routes = [
+  { path: 'products', component: ProductsListComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'products/:id/edit', component: ProductEditComponent },
+  // Add more routes as needed
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProductsRoutingModule { }
