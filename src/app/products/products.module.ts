6a1ff501847ddf9febfd7b681e45004dd68c5ae2
ghiberti85 <ghiberti85@gmail.com>
2024-05-07 23:59:48 -0300

    Products Module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsListComponent } from './product-list/produtct-list.component';
+import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
     ProductEditComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductsRoutingModule
   ]
 })
 export class ProductsModule { }
