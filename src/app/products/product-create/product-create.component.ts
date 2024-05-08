a01a439e2a9ac5348a4fcc06934fdef0ee18d93c
ghiberti85 <ghiberti85@gmail.com>
2024-05-08 00:30:00 -0300

    Create Product Component

--- src/app/products/product-create/product-create.component.ts
+++ src/app/products/product-create/product-create.component.ts
@@ -6,7 +6,7 @@ import { Product } from '../product.model';
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
-  styleUrls: ['/product-create.component.scss']
+  styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent {
   product: Product = { id: 0, name: '', price: 0, description: '' };
